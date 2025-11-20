df2beb7b1ddb96adba673b0fda55f2106b4608d5
Adam French <adam.a.french@outlook.com>
2025-11-20 22:13:58 +0000

    adding backend port env var to nginx

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -4,10 +4,10 @@ set -e
 # Check if certificate exists
 if [ -f "/etc/letsencrypt/live/$DOMAIN/fullchain.pem" ] && [ -f "/etc/letsencrypt/live/$DOMAIN/privkey.pem" ]; then
     echo "Certificates found. Using production nginx config."
-    envsubst '$BACKEND_HOST $DOMAIN' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+    envsubst '$BACKEND_HOST $BACKEND_PORT $DOMAIN' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 else
     echo "Certificates NOT found. Using setup nginx config."
-    envsubst '$BACKEND_HOST $DOMAIN' < /etc/nginx/nginx_setup.conf.template > /etc/nginx/nginx.conf
+    envsubst '$BACKEND_HOST $BACKEND_PORT $DOMAIN' < /etc/nginx/nginx_setup.conf.template > /etc/nginx/nginx.conf
 fi
 
 # Start nginx
