d042f365cfeb04e792290965d1a40c32f2d8f0a1
Adam French <adam.a.french@outlook.com>
2025-11-23 21:33:40 +0000

    plenty of spotify variables

--- backend/handlers/store.go
+++ backend/handlers/store.go
@@ -1,13 +1,15 @@
 package handlers
 
 import (
+	"github.com/zmb3/spotify/v2"
 	spotifyauth "github.com/zmb3/spotify/v2/auth"
 	"golang.org/x/oauth2"
 	"gorm.io/gorm"
 )
 
 type Store struct {
-	DB           *gorm.DB
-	SpotifyAuth  *spotifyauth.Authenticator
-	SpotifyToken *oauth2.Token
+	DB            *gorm.DB
+	SpotifyAuth   *spotifyauth.Authenticator
+	SpotifyToken  *oauth2.Token
+	SpotifyClient *spotify.Client
 }
