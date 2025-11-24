5589bf60ab09e0f68005e286d01f271ba3058ea1
Adam French <adam.a.french@outlook.com>
2025-11-24 14:13:10 +0000

    adding icecast reverse proxy

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 # Check if certificate exists
 if [ -f "/etc/letsencrypt/live/$DOMAIN/fullchain.pem" ] && [ -f "/etc/letsencrypt/live/$DOMAIN/privkey.pem" ]; then
     echo "Certificates found. Using production nginx config."
-    envsubst '$BACKEND_HOST $BACKEND_PORT $DOMAIN' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+    envsubst '$BACKEND_HOST $BACKEND_PORT $DOMAIN $ICECAST_PORT $ICECAST_HOST' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 else
     echo "Certificates NOT found. Using setup nginx config."
     envsubst '$BACKEND_HOST $BACKEND_PORT $DOMAIN' < /etc/nginx/nginx_setup.conf.template > /etc/nginx/nginx.conf
