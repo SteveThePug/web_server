efd3b75673b597a8aff779b16d4f47733dc68a9f
Adam French <adam.a.french@outlook.com>
2025-11-24 14:14:40 +0000

    don't need to declare env vars

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -4,10 +4,10 @@ set -e
 # Check if certificate exists
 if [ -f "/etc/letsencrypt/live/$DOMAIN/fullchain.pem" ] && [ -f "/etc/letsencrypt/live/$DOMAIN/privkey.pem" ]; then
     echo "Certificates found. Using production nginx config."
-    envsubst '$BACKEND_HOST $BACKEND_PORT $DOMAIN $ICECAST_PORT $ICECAST_HOST' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+    envsubst < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 else
     echo "Certificates NOT found. Using setup nginx config."
-    envsubst '$BACKEND_HOST $BACKEND_PORT $DOMAIN' < /etc/nginx/nginx_setup.conf.template > /etc/nginx/nginx.conf
+    envsubst < /etc/nginx/nginx_setup.conf.template > /etc/nginx/nginx.conf
 fi
 
 # Start nginx
