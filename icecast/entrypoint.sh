c16e405318f12c0de6f0b9ecb123461f63709c3a
Adam French <adam.a.french@outlook.com>
2025-11-24 13:18:35 +0000

    new icecast image

--- icecast/entrypoint.sh
+++ icecast/entrypoint.sh
@@ -4,5 +4,7 @@ set -e
 # Substitute environment variables into template
 envsubst < /etc/icecast2/icecast.xml.template > /etc/icecast2/icecast.xml
 
+echo "test"
+
 # Run icecast with the generated config
 exec icecast -c /etc/icecast2/icecast.xml
