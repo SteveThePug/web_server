f14cc26456f40f266837a9c052fbe9d05e807473
Adam French <adam.a.french@outlook.com>
2025-11-24 16:27:04 +0000

    fix envsubst

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -4,10 +4,12 @@ set -e
 # Check if certificate exists
 if [ -f "/etc/letsencrypt/live/$DOMAIN/fullchain.pem" ] && [ -f "/etc/letsencrypt/live/$DOMAIN/privkey.pem" ]; then
     echo "Certificates found. Using production nginx config."
-    envsubst < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+    envsubst '${DOMAIN} ${BACKEND_HOST} ${BACKEND_PORT} ${ICECAST_HOST} ${ICECAST_PORT}' \
+        < /etc/nginx/nginx.conf.template \
+        > /etc/nginx/nginx.conf
 else
     echo "Certificates NOT found. Using setup nginx config."
-    envsubst < /etc/nginx/nginx_setup.conf.template > /etc/nginx/nginx.conf
+    envsubst '${DOMAIN}' < /etc/nginx/nginx_setup.conf.template > /etc/nginx/nginx.conf
 fi
 
 # Start nginx
