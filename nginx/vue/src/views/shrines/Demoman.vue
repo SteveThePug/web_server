891b6242a961afdecca07fc3cb9f44e951dba1a3
Adam French <adam.a.french@outlook.com>
2025-11-28 00:48:48 +0000

    adding content

--- nginx/vue/src/views/shrines/Demoman.vue
+++ nginx/vue/src/views/shrines/Demoman.vue
@@ -1,1 +1,31 @@
-<template></template>
+<template>
+    <div>
+        <video width="600" height="400" controls preload="none">
+            <source
+                src="/img/demoman/1760582395316219.webm"
+                type="video/webm"
+            />
+        </video>
+        <video width="600" height="400" controls preload="none">
+            <source
+                src="/img/demoman/1761052136609718.webm"
+                type="video/webm"
+            />
+        </video>
+        <video width="600" height="400" controls preload="none">
+            <source src="/img/demoman/1761088452011210.mp4" type="video/mp4" />
+        </video>
+        <video width="600" height="400" controls preload="none">
+            <source
+                src="/img/demoman/1761570214170465.webm"
+                type="video/webm"
+            />
+        </video>
+        <video width="600" height="400" controls preload="none">
+            <source
+                src="/img/demoman/1761828457509465.webm"
+                type="video/webm"
+            />
+        </video>
+    </div>
+</template>
