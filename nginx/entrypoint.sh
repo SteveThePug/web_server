bf574fa34fded4ba5b3dd889c5c8f144f9425961
Adam French <adam.a.french@outlook.com>
2025-11-30 01:40:32 +0000

    adding endpoint reference

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 # Check if certificate exists
 if [ -f "/etc/letsencrypt/live/$DOMAIN/fullchain.pem" ] && [ -f "/etc/letsencrypt/live/$DOMAIN/privkey.pem" ]; then
     echo "Certificates found. Using production nginx config."
-    envsubst '${DOMAIN} ${BACKEND_HOST} ${BACKEND_PORT} ${ICECAST_HOST} ${ICECAST_PORT}' \
+    envsubst '${DOMAIN} ${BACKEND_HOST} ${BACKEND_PORT} ${BACKEND_ENDPOINT} ${ICECAST_HOST} ${ICECAST_PORT}' \
         < /etc/nginx/nginx.conf.template \
         > /etc/nginx/nginx.conf
 else
