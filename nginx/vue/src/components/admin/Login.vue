fb47883e5946ff70556fb54e699334112e490199
Adam French <adam.a.french@outlook.com>
2025-12-09 17:27:26 +0000

    deleting unused components and restructuring

---
+++ nginx/vue/src/components/admin/Login.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { ref, onMounted } from "vue";
+
+const loggedIn = ref(false);
+</script>
+
+<template>
+    <div v-if="!loggedIn">
+        <h1>login</h1>
+        <textarea>Username</textarea>
+        <textarea>Password</textarea>
+    </div>
+</template>
---
+++ nginx/vue/src/components/borders/UtenaFrame.vue
@@ -0,0 +1,50 @@
+<template>
+    <img src="/img/borders/utena.png" class="flower tl antirotate" />
+    <img src="/img/borders/utena.png" class="flower tr rotate" />
+    <img src="/img/borders/utena.png" class="flower bl rotate" />
+    <img src="/img/borders/utena.png" class="flower br antirotate" />
+    <slot />
+</template>
+
+<style scoped>
+.flower {
+    position: absolute;
+    width: 150px;
+}
+.tl {
+    top: 100px;
+    left: 0px;
+    --start: 0deg;
+}
+.tr {
+    top: 100px;
+    right: 0;
+    --start: 90deg;
+}
+.bl {
+    bottom: -100px;
+    left: 0;
+    --start: 180deg;
+}
+.br {
+    bottom: -100px;
+    right: 0;
+    --start: 270deg;
+}
+.rotate {
+    animation: spin 3s linear infinite;
+}
+
+.antirotate {
+    animation: spin 3s linear infinite reverse;
+}
+
+@keyframes spin {
+    from {
+        transform: rotate(var(--start));
+    }
+    to {
+        transform: rotate(calc(var(--start) + 360deg));
+    }
+}
+</style>
