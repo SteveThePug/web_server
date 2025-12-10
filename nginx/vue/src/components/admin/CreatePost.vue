95569c8e4308ff8541769f33800420c5ffe6d8f0
Adam French <adam.a.french@outlook.com>
2025-12-10 04:43:38 +0000

    renaming gifs, adding create post element

---
+++ nginx/vue/src/components/admin/CreatePost.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { ref, onMounted, computed } from "vue";
+import axios from "axios";
+import { useAuthStore } from "@/stores/auth";
+
+const auth = useAuthStore();
+const title = ref("");
+const content = ref("");
+
+async function post() {
+    try {
+        const res = await axios.post("/api/posts", {
+            title: title.value,
+            content: content.value,
+        });
+        title.value = "";
+        content.value = "";
+        console.log(res.data);
+    } catch (err) {
+        console.error(err);
+    }
+}
+</script>
+
+<template>
+    <div v-if="auth.loggedIn">
+        <h1>Create Post</h1>
+        <input type="text" v-model="title" placeholder="Title" />
+        <textarea v-model="content" placeholder="Content"></textarea>
+        <button @click="post">Upload</button>
+    </div>
+</template>
