5e77b480f783b2ae7fde0b8f50ea078b232ec7ce
Adam French <adam.a.french@outlook.com>
2025-12-10 23:04:43 +0000

    moving models to one file for convenience

--- backend/models/models.go
+++ backend/models/models.go
@@ -6,6 +6,16 @@ import (
 	"gorm.io/gorm"
 )
 
+type User struct {
+	ID        uint           `gorm:"primarykey" json:"id"`
+	CreatedAt time.Time      `json:"createdAt"`
+	UpdatedAt time.Time      `json:"updatedAt"`
+	DeletedAt gorm.DeletedAt `gorm:"index" json:"deletedAt"`
+	Username  string         `gorm:"uniqueIndex" json:"username"`
+	Password  []byte         `json:"-"`
+	Admin     bool           `json:"admin"`
+}
+
 type Post struct {
 	ID        uint           `gorm:"primarykey" json:"id"`
 	CreatedAt time.Time      `json:"createdAt"`
