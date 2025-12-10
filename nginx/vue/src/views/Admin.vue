809c762e43c7ea998c2bd01c34b76f41da97252a
Adam French <adam.a.french@outlook.com>
2025-12-10 04:44:40 +0000

    adding new elements

--- nginx/vue/src/views/Admin.vue
+++ nginx/vue/src/views/Admin.vue
@@ -1,11 +1,15 @@
 <script setup>
 import Login from "@/components/admin/Login.vue";
+import CreateUser from "@/components/admin/CreateUser.vue";
+import CreatePost from "@/components/admin/CreatePost.vue";
 </script>
 
 <template>
     <main class="center-content flex-col">
         <div class="a5page-portrait bdr-1 grid">
             <Login />
+            <CreateUser />
+            <CreatePost />
         </div>
     </main>
 </template>
