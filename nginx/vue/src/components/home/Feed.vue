aa4402294b3fe978f8694436ae4478eda071e7b2
Adam French <adam.a.french@outlook.com>
2025-12-10 16:48:17 +0000

    added posts

--- nginx/vue/src/components/home/Feed.vue
+++ nginx/vue/src/components/home/Feed.vue
@@ -2,14 +2,32 @@
 import { ref, onMounted } from "vue";
 import axios from "axios";
 
-const posts = ref([]);
+const post = ref(null);
+const fetched = ref(false);
+let posts = [];
+let idx = 0;
+let len = 0;
+
 async function fetchPosts() {
     try {
         const res = await axios.get("https://adam-french.co.uk/api/posts");
         posts.value = res.data;
+        fetched.value = true;
+        post.value = posts[0];
+        len = posts.length;
     } catch (err) {
         console.error(err);
     }
+}
+
+function nextPost() {
+    post.value = posts[idx];
+    idx = (idx + 1) % len;
+}
+
+function prevPost() {
+    post.value = posts[idx];
+    idx = (idx - 1) % len;
 }
 
 onMounted(() => {
@@ -18,13 +36,15 @@ onMounted(() => {
 </script>
 
 <template>
-    <div v-for="post in posts">
+    <div>
         <h2>{{ post.title }}</h2>
         <p>By: {{ post.author.username }}</p>
         <div>{{ post.content }}</div>
         <small
             >Created at: {{ new Date(post.CreatedAt).toLocaleString() }}</small
         >
+        <button @click="nextPost">Next</button>
+        <button @click="prevPost">Prev</button>
     </div>
 </template>
 
