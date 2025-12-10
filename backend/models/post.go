d39d5c23c45897b048bc654acda90a60047322ff
Adam French <adam.a.french@outlook.com>
2025-12-10 17:30:40 +0000

    changing field names

--- backend/models/post.go
+++ backend/models/post.go
@@ -1,17 +1,18 @@
 package models
 
 import (
-	"database/sql"
 	"time"
+
+	"gorm.io/gorm"
 )
 
 type Post struct {
-	ID        uint         `gorm:"primarykey" json:"id"`
-	CreatedAt time.Time    `json:"createdAt"`
-	UpdatedAt time.Time    `json:"updatedAt"`
-	DeletedAt sql.NullTime `gorm:"index" json:"deletedAt"`
-	Title     string       `gorm:"not null" json:"title"`
-	AuthorID  uint         `json:"-"`
-	Author    *User        `gorm:"foreignKey:AuthorID" json:"author"`
-	Content   string       `json:"content"`
+	ID        uint           `gorm:"primarykey" json:"id"`
+	CreatedAt time.Time      `json:"createdAt"`
+	UpdatedAt time.Time      `json:"updatedAt"`
+	DeletedAt gorm.DeletedAt `gorm:"index" json:"deletedAt"`
+	Title     string         `gorm:"not null" json:"title"`
+	AuthorID  uint           `json:"-"`
+	Author    *User          `gorm:"foreignKey:AuthorID" json:"author"`
+	Content   string         `json:"content"`
 }
--- backend/models/user.go
+++ backend/models/user.go
@@ -1,16 +1,17 @@
 package models
 
 import (
-	"database/sql"
 	"time"
+
+	"gorm.io/gorm"
 )
 
 type User struct {
-	ID        uint         `gorm:"primarykey" json:"id"`
-	CreatedAt time.Time    `json:"createdAt"`
-	UpdatedAt time.Time    `json:"updatedAt"`
-	DeletedAt sql.NullTime `gorm:"index" json:"deletedAt"`
-	Username  string       `gorm:"uniqueIndex" json:"username"`
-	Password  []byte       `json:"-"`
-	Admin     bool         `json:"admin"`
+	ID        uint           `gorm:"primarykey" json:"id"`
+	CreatedAt time.Time      `json:"createdAt"`
+	UpdatedAt time.Time      `json:"updatedAt"`
+	DeletedAt gorm.DeletedAt `gorm:"index" json:"deletedAt"`
+	Username  string         `gorm:"uniqueIndex" json:"username"`
+	Password  []byte         `json:"-"`
+	Admin     bool           `json:"admin"`
 }
