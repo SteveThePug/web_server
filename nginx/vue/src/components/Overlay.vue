21f173b95ae4bec973e6927ccfcacbe70ac4ec6f
Adam French <adam.a.french@outlook.com>
2025-12-16 03:42:24 +0000

    ok

---
+++ nginx/vue/src/components/Overlay.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="overlay">
+        <slot />
+    </div>
+</template>
+
+<style scoped>
+.overlay {
+    position: absolute;
+    inset: 0; /* full width & height of parent */
+    pointer-events: none; /* overlay does not block clicks */
+}
+</style>
