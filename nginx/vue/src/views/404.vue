55509802a5078d63deecb81472118133ab320ac9
Adam French <adam.a.french@outlook.com>
2026-01-08 20:31:47 +0000

    mogging 404

--- nginx/vue/src/views/404.vue
+++ nginx/vue/src/views/404.vue
@@ -1,7 +1,13 @@
 <template>
-    <main>
-        <h1>404 not found lol</h1>
-        <img href="/img/epic.jpeg" ) />
-        <RouterLink to="/">Take me home!</RouterLink>
+    <main class="flex-col center-content">
+        <div
+            class="a4page-portrait center-content bdr-1 flex-col relative scroll-y"
+        >
+            <h1>404</h1>
+            <RouterLink to="/" class="bdr-2">
+                <img src="/img/memes/epic.jpeg" />
+            </RouterLink>
+            <h1>Click her, she will take you home</h1>
+        </div>
     </main>
 </template>
