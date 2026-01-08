dfbd4265ca7e51aeb0beb2d122d61e7443181db4
Adam French <adam.a.french@outlook.com>
2026-01-08 20:29:53 +0000

    add up directory button to nav

--- nginx/vue/src/components/Navbar.vue
+++ nginx/vue/src/components/Navbar.vue
@@ -1,9 +1,26 @@
+<script setup>
+import { computed } from "vue";
+import { useRoute } from "vue-router";
+
+const route = useRoute();
+
+const parentPath = computed(() => {
+    const segments = route.path.split("/").filter(Boolean);
+    if (segments.length == 1) {
+        return "/";
+    } else {
+        segments.pop();
+        return segments.length ? "/" + segments.join("/") : null;
+    }
+});
+</script>
+
 <template>
     <nav class="flex-row">
-        <RouterLink class="bdr-primary margin1" to="/"
-            ><h1>HOME</h1></RouterLink
-        >
-        <!-- <RouterLink to="/cv"><h1>CV</h1></RouterLink> -->
+        <RouterLink class="bdr-2" to="/"><h1>HOME</h1></RouterLink>
+        <RouterLink class="bdr-2" v-if="parentPath" :to="parentPath">
+            <h1>UP</h1>
+        </RouterLink>
     </nav>
 </template>
 
