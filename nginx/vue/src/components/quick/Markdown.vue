0c955b65f8c486d1aba420da006d493f849288d6
Adam French <adam.a.french@outlook.com>
2026-01-09 09:37:01 +0000

    markdown support

---
+++ nginx/vue/src/components/quick/Markdown.vue
@@ -0,0 +1,21 @@
+<script setup>
+import MarkdownIt from "markdown-it";
+import { katex } from "@mdit/plugin-katex";
+
+const mdIt = MarkdownIt().use(katex);
+
+const props = defineProps({
+    source: String,
+});
+</script>
+
+<template>
+    <div
+        v-html="mdIt.render(props.source)"
+        class="flex-col center-content"
+    ></div>
+</template>
+
+<style>
+@import "katex/dist/katex.min.css";
+</style>
