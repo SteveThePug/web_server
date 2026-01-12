d4671ecd96613c6bf6545b3a1fc01c488d28d182
Adam French <adam.a.french@outlook.com>
2026-01-12 16:26:40 +0000

    adding obsidian support

--- nginx/vue/src/views/Notes.vue
+++ nginx/vue/src/views/Notes.vue
@@ -1,13 +1,20 @@
 <script setup>
-import RouterTable from "@/components/quick/RouterTable.vue";
-const res = await axios.post("/api/notes/Welcome");
+import Markdown from "@/components/quick/Markdown.vue";
+import { ref, onMounted } from "vue";
+import axios from "axios";
+
+const note = ref(await axios.post("/api/notes/Welcome"));
+
+function fetchNote() {}
 </script>
 
 <template>
     <main class="center-content flex-col">
         <div class="background halftone" />
         <div class="a4page-portrait bdr-1 flex-col relative scroll-y gap">
-            <RouterTable :linkArr="note_links" />
+            <h1>{{ note.title }}</h1>
+            <small>{{ note.last_edited }}</small>
+            <Markdown class="fill wrap" :source="note.contents" />
         </div>
     </main>
 </template>
