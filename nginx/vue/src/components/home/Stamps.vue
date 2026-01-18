c12779214a76b708f4c9ce2889565f1ed8dcfd3a
Adam French <adam.a.french@outlook.com>
2026-01-18 08:45:08 +0000

    added bro stamp

--- nginx/vue/src/components/home/Stamps.vue
+++ nginx/vue/src/components/home/Stamps.vue
@@ -1,15 +1,21 @@
 <template>
     <div class="flex-col center-content">
-        <div class="flex-row">
-            <img src="/img/stamps/portal.gif" />
+        <div class="flex-col">
+            <!-- <img src="/img/stamps/portal.gif" />
             <img src="/img/stamps/miku.gif" />
-            <img src="/img/stamps/utau.gif" />
-            <a href="adam-french.co.uk">
-                <img src="/img/stamps/mine.png" />
+            <img src="/img/stamps/utau.gif" /> -->
+            <a href="https://www.adam-french.co.uk">
+                <img src="/img/stamps/mine.gif" />
             </a>
-        </div>
-        <div>
-            <p>more soon (will diy some)</p>
+            <div class="flex-row center-content">
+                <p>--> hit up bro --></p>
+                <a href="https://jacobbarron.xyz">
+                    <img
+                        src="https://jacobbarron.xyz/Banneh.gif"
+                        alt="jacobbarron.xyz"
+                    />
+                </a>
+            </div>
         </div>
     </div>
 </template>
