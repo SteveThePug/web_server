a0ac87fd60505ae3f7ed39c687222ce47b29cae6
Adam French <adam.a.french@outlook.com>
2026-02-06 13:40:47 +0000

    favorites and other should be done?

--- nginx/vue/src/components/home/Consumption.vue
+++ nginx/vue/src/components/home/Consumption.vue
@@ -1,4 +1,6 @@
 <script setup>
+import { onBeforeMount } from "vue";
+
 import AutoScroll from "@/components/util/AutoScroll.vue";
 import OptionalLinkTable from "@/components/util/OptionalLinkTable.vue";
 import Header from "@/components/text/Header.vue";
@@ -6,13 +8,17 @@ import Header from "@/components/text/Header.vue";
 import { useActivityStore } from "@/stores/activity";
 
 const activityStore = useActivityStore();
+
+onBeforeMount(() => {
+    activityStore.fetchActivity();
+});
 </script>
 
 <template>
     <div class="flex flex-col items-center">
         <Header>Consumption</Header>
-        <AutoScroll>
-            <OptionalLinkTable :data="activityStore.activity" />
+        <AutoScroll class="flex-1 w-full">
+            <OptionalLinkTable class="w-full" :data="activityStore.activity" />
         </AutoScroll>
     </div>
 </template>
--- nginx/vue/src/components/home/Favorites.vue
+++ nginx/vue/src/components/home/Favorites.vue
@@ -1,4 +1,6 @@
 <script setup>
+import { onBeforeMount } from "vue";
+
 import Header from "@/components/text/Header.vue";
 import OptionalLinkTable from "@/components/util/OptionalLinkTable.vue";
 import AutoScroll from "@/components/util/AutoScroll.vue";
@@ -6,6 +8,10 @@ import AutoScroll from "@/components/util/AutoScroll.vue";
 import { useFavoritesStore } from "@/stores/favorites";
 
 const favoritesStore = useFavoritesStore();
+
+onBeforeMount(() => {
+    favoritesStore.fetchFavorites();
+});
 </script>
 
 <template>
