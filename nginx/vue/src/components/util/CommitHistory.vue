8c2e9ba9a5af1956974a90352d4c4a7cff337442
Adam French <adam.a.french@outlook.com>
2026-03-25 03:13:56 +0000

    Remove horizontal scroll from commit history (sry bad claude naughty)

--- nginx/vue/src/components/util/CommitHistory.vue
+++ nginx/vue/src/components/util/CommitHistory.vue
@@ -9,26 +9,29 @@ const { gitFeed: feed, loaded } = storeToRefs(homeData);
 </script>
 
 <template>
-  <div class="flex flex-col text-center min-h-0 h-full overflow-x-hidden">
-    <Header class="text-left">Commits</Header>
+    <div class="flex flex-col text-center min-h-0 h-full overflow-x-hidden">
+        <Header class="text-left">Commits</Header>
 
-    <div v-if="!loaded" class="flex-1 overflow-y-auto">
-      <p>Loading latest activity...</p>
-    </div>
+        <div v-if="!loaded" class="flex-1 overflow-y-auto">
+            <p>Loading latest activity...</p>
+        </div>
 
-    <div v-else-if="feed" class="flex-1 flex flex-col overflow-y-auto">
-      <h3>Last git activity</h3>
-      <img :src="feed.avatarUrl" alt="User avatar" class="avatar" />
-      <Link :href="feed.repoUrl">
-        <h3>repo: {{ feed.repoName }}</h3>
-      </Link>
-      <p>Action: {{ feed.opType }}</p>
-      <p>Message: {{ feed.commitMessage }}</p>
-      <small> {{ new Date(feed.createdAt).toLocaleString() }}</small>
-    </div>
+        <div
+            v-else-if="feed"
+            class="flex-1 flex flex-col overflow-y-auto overflow-x-hidden"
+        >
+            <h3>Last git activity</h3>
+            <img :src="feed.avatarUrl" alt="User avatar" class="avatar" />
+            <Link :href="feed.repoUrl">
+                <h3>repo: {{ feed.repoName }}</h3>
+            </Link>
+            <p>Action: {{ feed.opType }}</p>
+            <p>Message: {{ feed.commitMessage }}</p>
+            <small> {{ new Date(feed.createdAt).toLocaleString() }}</small>
+        </div>
 
-    <div v-else class="flex-1 overflow-y-auto">
-      <p>No activity found.</p>
+        <div v-else class="flex-1 overflow-y-auto">
+            <p>No activity found.</p>
+        </div>
     </div>
-  </div>
 </template>
