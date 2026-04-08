1d472d382bcad554403731e92dc16bb052e526fb
Adam French <adam.a.french@outlook.com>
2026-04-08 01:33:26 +0100

    Remove padding beneath header

--- vue/src/components/text/Header.vue
+++ vue/src/components/text/Header.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="w-full border-b border-primary pb-2">
+    <div class="w-full border-b border-primary pb-0.5">
         <h1 class="p-0 m-0">
             <slot />
         </h1>
